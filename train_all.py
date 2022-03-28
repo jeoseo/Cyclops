45f29973a1717706d8d808872acf403e2e9d3a3f
jeoseo <jeongwoo.seo@vanderbilt.edu>
2022-03-27 19:42:58 -0500

    gitignore

--- train_all.py
+++ train_all.py
@@ -3,11 +3,12 @@
 
 import sys
 sys.path.append('/scratch/seoj4/installations')
+import os
 
 def main():
     #run on GLP
-    os.system("python ./GLPDepth/code/train.py --dataset nyudepthv2 --data_path ./GLPDepth/datasets/ --max_depth 10.0 --max_depth_eval 10.0  ")
+    glp_runstring="python ./GLPDepth/code/train.py --dataset nyudepthv2 --data_path ./GLPDepth/datasets/ --batch_size 12 --max_depth 10.0 --max_depth_eval 10.0  --gpu_or_cpu cpu --save_result --save_model"
+    os.system(glp_runstring)
 
-    
 if __name__ == '__main__':
     main()
